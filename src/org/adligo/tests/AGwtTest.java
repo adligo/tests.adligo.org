bbd7b63b9118f19c89d9c5199074badff6087732
scott <scott@adligo.com>
2009-11-06 05:54:46 +0000

    working on GwtTests

---
+++ src/org/adligo/tests/AGwtTest.java
@@ -0,0 +1,11 @@
+package org.adligo.tests;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public abstract class AGwtTest extends GWTTestCase {
+
+	public AGwtTest() {
+		super();
+	}
+
+}
---
+++ src/org/adligo/tests/InitGwt.java
@@ -0,0 +1,23 @@
+package org.adligo.tests;
+
+import org.adligo.gwt.util.client.GwtPlatform;
+import org.adligo.i.log.client.LogPlatform;
+
+public class InitGwt {
+	private static final boolean init = false;
+	  
+	  public static synchronized void init(){
+		  if (!init) {
+			try {
+				GwtPlatform.init();
+				LogPlatform.init();
+			} catch (Exception x) {
+				x.printStackTrace();
+			}
+		  } 
+	  }
+	  
+	  static {
+		  init();
+	  }
+}
