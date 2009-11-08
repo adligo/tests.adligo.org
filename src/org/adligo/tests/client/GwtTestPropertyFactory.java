0d6e462ea1a8ff4a8b5edb643c4dc5c143c71e80
scott <scott@adligo.com>
2009-11-08 08:40:40 +0000

    may be part of issue causing eclipse to fail the GwtTests in ModelsCoreTests?

--- src/org/adligo/tests/client/GwtTestPropertyFactory.java
+++ src/org/adligo/tests/client/GwtTestPropertyFactory.java
@@ -12,16 +12,22 @@ import org.adligo.i.util.client.ListenerValueObject;
 import com.google.gwt.core.client.GWT;
 
 public class GwtTestPropertyFactory extends GwtPropertyFactory {
-
+	private static Map<String,String> adligo_log_properties;
+	
 	@SuppressWarnings("unchecked")
 	public Object createNew(Object p) {
+		
+		
 		ListenerValueObject list = (ListenerValueObject) p;
 		System.out.println("Loading property file org.adligo.tests.client.adligo_log.properties from classpath");
 		
 	      Event event = new Event();
-		  
-	      Map<String,String> props = (Map) GWT.create(adligo_log.class) ;
-		  I_Map result = new MapWrapper(props);
+		
+	      if (adligo_log_properties == null) {
+	    	  Map<String,String> props = (Map) GWT.create(adligo_log.class) ;
+	    	  adligo_log_properties = props;
+	      }
+		  I_Map result = new MapWrapper(adligo_log_properties);
 		  
 		  event.setValue(result);
 			
